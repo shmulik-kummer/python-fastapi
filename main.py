930fa9eaceecdd56eb52c8a366384a4e0eadc4d4
Shmulik Kummer <kummersh@gmail.com>
2024-04-29 16:58:49 +0300

    Added post route

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+from fastapi import Body, FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": payLoad}
